e7b5f35066d0c7d3af645320a9b4390546fc1cd7
Twanst Codes <98418359+TwanstDeveloper@users.noreply.github.com>
2023-02-27 21:59:23 +0300

    code file

---
+++ caeaser/caeaser/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caeaser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "ATTACKon   TITAN";
+            int key = 3;
+            Program ob = new Program();
+            string x= ob.encription(txt,key);
+            Console.WriteLine(x);
+            Console.WriteLine("\n");
+            string v = ob.decription(x,key);
+            Console.WriteLine(v);
+            Console.ReadKey();
+
+        }
+        string encription(string txt, int key)
+        {
+            string r = "";
+            for (int i = 0; i < txt.Length; i++)
+            {
+               
+                if (char.IsUpper(txt[i]))
+                {
+                    r = r + (char)((int)(txt[i] + key - 65)%26+65);
+                }
+                else
+                {
+                    r = r + (char)((int)(txt[i]+key-97)%26+97);
+                }
+
+
+            }
+            return r;
+        }
+        string decription(string txt, int key)
+        {
+            string r = "";
+            for (int i = 0; i < txt.Length; i++)
+            {
+
+                if (char.IsUpper(txt[i]))
+                {
+                    r = r + (char)((int)(txt[i] - key - 65) % 26 + 65);
+                }
+                else
+                {
+                    r = r + (char)((int)(txt[i] - key - 97) % 26 + 97);
+                }
+
+
+            }
+            return r;
+        }
+    }
+}
